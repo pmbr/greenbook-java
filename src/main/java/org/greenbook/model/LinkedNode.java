263c15c5813931ecb787e4cb6e0e74ff4f602509
Paulo Rezende <paulo@pmbr.net>
2020-04-22 15:57:59 -0700

    linked list node

---
+++ src/main/java/org/greenbook/model/LinkedNode.java
@@ -0,0 +1,24 @@
+package org.greenbook.model;
+
+public class LinkedNode<T> {
+
+    private T value;
+    private LinkedNode<T> next;
+
+    public LinkedNode(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return this.value;
+    }
+
+    public LinkedNode<T> getNext() {
+        return next;
+    }
+
+    public void setNext(LinkedNode<T> next) {
+        this.next = next;
+    }
+
+}
