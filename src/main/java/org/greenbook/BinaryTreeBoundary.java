3adca14728064ad84688e30b0d737bdc7697c639
Paulo Rezende <paulo@pmbr.net>
2020-04-24 08:42:07 -0700

    binary tree boundaries

---
+++ src/main/java/org/greenbook/BinaryTreeBoundary.java
@@ -0,0 +1,45 @@
+package org.greenbook;
+
+import org.greenbook.model.BinaryTreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreeBoundary {
+
+    public static final int ROOT = 0;
+    public static final int LEFT = -1;
+    public static final int RIGHT = 1;
+
+    public static <T> String boundaries(BinaryTreeNode<T> root) {
+        List<T> boundaries = new ArrayList<>();
+        findNextBoundaries(root, 0, 0, boundaries);
+        String output = "";
+        for (T element: boundaries) {
+            output += element.toString() + " ";
+        }
+
+        return output.trim();
+    }
+
+    private static <T> void findNextBoundaries(BinaryTreeNode<T> node, int currentDirection, int originalDirection, List<T> boundaries) {
+        if ( (node.getLeft() == null && node.getRight() == null)) {
+            boundaries.add(node.getValue());
+            return;
+        }
+        if (currentDirection == ROOT || (currentDirection == LEFT && currentDirection == originalDirection)) {
+            boundaries.add(node.getValue());
+        }
+        if (node.getLeft() != null) {
+            findNextBoundaries(node.getLeft(), LEFT, originalDirection == ROOT ? LEFT : originalDirection, boundaries);
+        }
+        if (node.getRight() != null) {
+            findNextBoundaries(node.getRight(), RIGHT, originalDirection == ROOT ? RIGHT : originalDirection, boundaries);
+        }
+        if (currentDirection == RIGHT && currentDirection == originalDirection) {
+            boundaries.add(node.getValue());
+        }
+    }
+
+}
+
---
+++ src/test/java/org/greenbook/BinaryTreeBoundaryTest.java
@@ -0,0 +1,27 @@
+package org.greenbook;
+
+import org.greenbook.model.BinaryTreeNode;
+import org.junit.Test;
+
+import static org.greenbook.BinaryTreeBoundary.boundaries;
+import static org.junit.Assert.assertEquals;
+
+public class BinaryTreeBoundaryTest {
+
+    @Test
+    public void testBoundaries() {
+        BinaryTreeNode<Integer> root = new BinaryTreeNode(20);
+        root.setLeft(new BinaryTreeNode<>(8));
+        root.getLeft().setLeft(new BinaryTreeNode<>(4));
+        root.getLeft().setRight(new BinaryTreeNode<>(12));
+        root.getLeft().getRight().setLeft(new BinaryTreeNode<>(10));
+        root.getLeft().getRight().setRight(new BinaryTreeNode<>(14));
+        root.setRight(new BinaryTreeNode<>(22));
+        root.getRight().setRight(new BinaryTreeNode<>(25));
+
+        String boundaries = boundaries(root);
+
+        assertEquals("20 8 4 10 14 25 22", boundaries);
+    }
+
+}
